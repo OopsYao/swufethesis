5e5cc1d0407b3519d74670ea0af0b38d0ba965cc
oopsy <whoops.yao@gmail.com>
2021-02-25 00:40:27 +0800

    test: 增加l3build单元测试

---
+++ build.lua
@@ -0,0 +1,6 @@
+module = 'swufethesis'
+-- sandbox为独立环境
+sourcefiles = {"*.dtx", "*.ins", "*-????-??-??.sty", "logo-Swufe.png", "backcover.pdf", "swufethesis.cls"}
+installfiles = {"*.cls", "*.bst", "logo-Swufe.png", "backcover.pdf"}
+checkengines = {"xetex"}
+stdengine = "xetex"
