2b036303fcf8694ed52bc646fc15308d3b57cb7e
oopsy <whoops.yao@gmail.com>
2021-03-03 19:09:52 +0800

    ci: 添加.ci-dock目录作为CI沙盒
    CI上临时下载的文件全部放入.ci-dock目录，便于统一缓存。

--- ci/latexindent.sh
+++ ci/latexindent.sh
@@ -3,9 +3,9 @@
 set -ev
 
 for f in $(find \
-  -path ./latexindent -prune -false \
+  -path ./.ci-dock -prune -false \
   -or -name '*.tex' \
   -or -name '*.cls' \
   -or -name '*.bib'); do
-  latexindent/latexindent.pl -l "$f" | diff "$f" -
+  .ci-dock/indent/latexindent.pl -l "$f" | diff "$f" -
 done
