23eeb5e6ec6908f8e9511ee2587a5562462beb8b
oopsy <whoops.yao@gmail.com>
2021-05-29 10:54:11 +0800

    test: 更新测试文件
    CTAN上的宏包有更新，之前的测试无法再通过，需要更新测试文件。

--- ci/texlive-path.sh
+++ ci/texlive-path.sh
@@ -6,7 +6,7 @@ if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
   # 下面sed使用`|`作为分割符
   echo "$(source ci/pwd.sh)/.ci-dock/tl/bin/win32" | sed 's|/|\\|g'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo $PWD/.ci-dock/tl/bin/x86_64-darwin
+  echo $PWD/.ci-dock/tl/bin/universal-darwin
 else
   echo $PWD/.ci-dock/tl/bin/x86_64-linux
 fi
